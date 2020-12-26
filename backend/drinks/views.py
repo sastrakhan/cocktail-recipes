47e406d522361edb8efb373636c02b78c0a48402
Kamille Norris <kamionorato@gmail.com>
2020-12-26 18:09:54 -0500

    Filtering, ordering, and searching

--- backend/drinks/views.py
+++ backend/drinks/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+import django_filters
+from rest_framework import filters, viewsets
 from .serializers import DrinksSerializer
 from .models import Drinks
 
@@ -7,3 +7,12 @@ from .models import Drinks
 class DrinksView(viewsets.ModelViewSet):
     serializer_class = DrinksSerializer
     queryset = Drinks.objects.all()
+    filter_backends = [
+        django_filters.rest_framework.DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    filterset_fields = ['strCategory']
+    ordering_fields = '__all__'
+    ordering = ['strDrink']
+    search_fields = ['strDrink', 'strIngredient1']
